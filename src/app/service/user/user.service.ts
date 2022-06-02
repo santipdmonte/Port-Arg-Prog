a3859ca45217ccb21ef9e49fe234049be8b7750b
santipdmonte <santiagopedemonte02@gmail.com>
2022-06-02 20:38:33 -0300

    agrgue acces-control a httpHeaders y modificque archivo json

--- src/app/service/user/user.service.ts
+++ src/app/service/user/user.service.ts
@@ -5,7 +5,8 @@ import { User } from '../../Models/User'
 
 const httpOptions = {
   headers: new HttpHeaders ({
-    'Content-Type':'application/json'
+    'Content-Type':'application/json',
+    'Access-Control-Allow-Origin': '*'
   })
 }
 
